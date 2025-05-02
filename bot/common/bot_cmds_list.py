fd435a2dfa8c0bb8605575c47770f0f1247f7fb5
Dmitry <157917126+NikZess@users.noreply.github.com>
2025-05-02 21:52:36 +0300

    commands list with description for bot

---
+++ bot/common/bot_cmds_list.py
@@ -0,0 +1,9 @@
+from aiogram.types import BotCommand
+
+private = [
+    BotCommand(command="start", description="Перезапустить бота 🤖"),
+    BotCommand(command="tasks", description="Просмотреть задачи 📔"),
+    BotCommand(command="daily_tasks", description="Просмотреть ежедневные задачи 📕"),
+    BotCommand(command="info", description="Информация и помощь ✏️"),
+    BotCommand(command="register", description="Зарегистрироваться ®️"),
+]
