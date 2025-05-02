3585dd4c8bfdec36e589282ea9033671eda45ce6
Dmitry <157917126+NikZess@users.noreply.github.com>
2025-05-02 21:45:00 +0300

    files of crud for users (interaction with database). funcs: search all users, search user by him username, delete all users, delete user by him username

---
+++ src/api/users/crud.py
@@ -0,0 +1,35 @@
+from sqlalchemy import select
+from fastapi import HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.engine import Result
+from ...core.models import User
+
+async def get_users(session: AsyncSession) -> list[User]:
+    query = select(User).order_by(User.id)
+    result = await session.execute(query)
+    users = result.scalars().all()
+    return list(users)
+
+async def get_user_by_username(
+    session: AsyncSession, 
+    username: str
+) -> User | None:
+    query = select(User).where(User.username == username)
+    result = await session.execute(query)
+    user = result.scalar_one_or_none()
+    return user
+
+async def delete_all_users(
+    session: AsyncSession,
+) -> None:
+    query = select(User).order_by(User.id)
+    await session.delete(query)
+    return {"message": "ok"}
+
+async def delete_user_by_username(
+    session: AsyncSession, 
+    user: User,    
+) -> None:
+    await session.delete(user)
+    await session.commit()
+    return {"message": "ok"}
