c28d91db82d263dcf259a6f961c97d2114ef0b45
Dmitry <157917126+NikZess@users.noreply.github.com>
2025-05-02 21:35:18 +0300

    the configuration file. classes with env variables

---
+++ src/core/config.py
@@ -0,0 +1,27 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+from pathlib import Path
+from pydantic_settings import BaseSettings
+
+load_dotenv(find_dotenv())
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+class Settings(BaseSettings):
+    db_url: str = f'sqlite+aiosqlite:///{BASE_DIR}/database.db'
+    db_echo: bool = True
+    
+class AdminData(BaseSettings):
+    admin_username: str = os.getenv("ADMIN_USERNAME")
+    admin_password: str = os.getenv("ADMIN_PASSWORD")
+    
+class JWTData(BaseSettings):
+    JWT_secret_key: str = os.getenv("JWT_SECRET_KEY")
+
+class BotToken(BaseSettings):
+    bot_token: str = os.getenv("7880169088:AAEC0f_wAV95lE3-axFMmwyQTukdvs9mEO4")
+
+settings = Settings()
+admin_data = AdminData()
+JWT_data = JWTData()
+bot_data = BotToken()
