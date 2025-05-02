b9ec04b5011fb770602d0c9e3df0c41b5a7d0357
Dmitry <157917126+NikZess@users.noreply.github.com>
2025-05-02 21:34:25 +0300

    the user class for the users table in the database. relation to the tasks table

---
+++ src/core/models/user.py
@@ -0,0 +1,15 @@
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import BigInteger, String
+from .base import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    telegram_id: Mapped[int] = mapped_column(BigInteger, unique=True, nullable=False)
+    full_name: Mapped[str] = mapped_column(String(255))
+    username: Mapped[str] = mapped_column(String(255), nullable=True)
+
+    tasks: Mapped[list["Task"]] = relationship(
+        back_populates="user", 
+        cascade="all, delete-orphan"
+    )
