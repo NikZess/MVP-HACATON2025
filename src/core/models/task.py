e53741916ed3bd6557bfa8ed8c579c40335c8116
Dmitry <157917126+NikZess@users.noreply.github.com>
2025-05-02 21:33:44 +0300

    the task class for the tasks's table in database. relation to the user table

---
+++ src/core/models/task.py
@@ -0,0 +1,11 @@
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey
+from .base import Base
+
+class Task(Base):
+    __tablename__ = "tasks"
+
+    username: Mapped[int] = mapped_column(ForeignKey("users.username"))
+    description: Mapped[str] = mapped_column()
+
+    user: Mapped["User"] = relationship(back_populates="tasks")
