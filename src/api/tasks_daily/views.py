ca42b6eeabe702ae110bd989ddfdab39c5af17be
Dmitry <157917126+NikZess@users.noreply.github.com>
2025-05-03 22:30:36 +0300

    daily tasks router

---
+++ src/api/tasks_daily/views.py
@@ -0,0 +1,74 @@
+from fastapi import APIRouter, Depends, status
+from sqlalchemy.ext.asyncio import AsyncSession
+from ...core.models.db_helper import get_async_session
+from . import crud
+from ..auth.authx_config import security
+
+router = APIRouter(
+    tags=["Daily Tasks"],
+)
+
+@router.post("/daily/",
+    summary="Добавить новую ежедневную задачу для пользователя",
+    description="Добавляет новую ежедневную задачу для пользователя по его username",
+    status_code=status.HTTP_201_CREATED,
+    dependencies=[Depends(security.access_token_required)]
+)   
+async def create_task_daily(
+    username: str,
+    description: str,
+    session: AsyncSession = Depends(get_async_session)
+):
+    return await crud.create_daily_task(
+        username=username,
+        description=description,
+        session=session
+    )
+
+@router.get("/daily/",
+    summary="Получить все ежедневные задачи",
+    description="Возвращает все ежедневные задачи пользователей из базы данных",
+    status_code=status.HTTP_200_OK,
+    dependencies=[Depends(security.access_token_required)],
+)
+async def get_all_tasks_daily(session: AsyncSession = Depends(get_async_session)):
+    return await crud.get_all_tasks_daily(session=session)
+
+@router.get("/daily/{username}/",
+    summary="Получить ежедневные задачи пользователя",
+    description="Возвращает все ежедневные задачи пользователя по его username",
+    status_code=status.HTTP_200_OK,
+    dependencies=[Depends(security.access_token_required)],
+)
+async def get_user_tasks_daily(
+    username: str,
+    session: AsyncSession = Depends(get_async_session)
+):
+    return await crud.get_user_tasks_daily(
+        username=username,
+        session=session,
+    )
+
+@router.delete("/daily/",
+    summary="Удалить все ежедневные задачи",
+    description="Удаляет все ежедневные задачи, всех пользователей из базы данных",
+    status_code=status.HTTP_204_NO_CONTENT,
+    dependencies=[Depends(security.access_token_required)],           
+)
+async def delete_all_tasks_daily(session: AsyncSession = Depends(get_async_session)):
+    return await crud.delete_all_tasks_daily(session=session)
+
+@router.delete("/daily/{username}/",
+    summary="Удалить ежедневные задачи пользователя",
+    description="Удаляет все ежедневные задачи пользователя по его username",
+    status_code=status.HTTP_204_NO_CONTENT,
+    dependencies=[Depends(security.access_token_required)],
+)
+async def delete_user_tasks_daily(
+    username: str,
+    session: AsyncSession = Depends(get_async_session)
+):
+    return await crud.delete_user_tasks_daily(
+        username=username,
+        session=session,
+    )
