cb107d56677de1b2ae6add67640ce16627580dd0
Dmitry <157917126+NikZess@users.noreply.github.com>
2025-05-03 22:25:00 +0300

    the start the app across run the uvicorn server

--- run.py
+++ run.py
@@ -1,4 +1,8 @@
 import uvicorn
 
 if __name__ == "__main__":
+<<<<<<< HEAD
     uvicorn.run("src.main:app", host="127.0.0.1", port=8000, reload=True)
+=======
+    uvicorn.run("src.main:app", host="127.0.0.1", port=5050, reload=True)
+>>>>>>> f93531d (the start the app across run the uvicorn server)
