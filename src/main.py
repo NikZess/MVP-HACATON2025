5a500f32e1d14579569690b5ccf208c46e696c64
Dmitry <157917126+NikZess@users.noreply.github.com>
2025-05-03 22:26:18 +0300

    main file of fastapi app, create database across asynccontextmanager, CORS, routers

--- src/main.py
+++ src/main.py
@@ -7,9 +7,17 @@ from contextlib import asynccontextmanager
 from .core.models import db_helper
 from .core.models import Base
 
+<<<<<<< HEAD
 from .api.tasks.views import router as tasks_router
 from .api.auth.views import router as auth_router
 from .api.users.views import router as users_router
+=======
+from .api.auth.views import router as auth_router
+from .api.users.views import router as users_router
+from .api.tasks.views import router as tasks_router
+from .api.tasks_daily.views import router as tasks_daily_router
+from .api.information.views import router as informations_router
+>>>>>>> 17224d8 (main file of fastapi app, create database across asynccontextmanager, CORS, routers)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -22,6 +30,11 @@ app = FastAPI(title="MVP Project", lifespan=lifespan)
 app.include_router(router=users_router, prefix="/api/v1/users")
 app.include_router(router=auth_router, prefix="/api/v1/auth")
 app.include_router(router=tasks_router, prefix="/api/v1/tasks")
+<<<<<<< HEAD
+=======
+app.include_router(router=tasks_daily_router, prefix="/api/v1/tasks")
+app.include_router(router=informations_router, prefix="/api/v1/information")
+>>>>>>> 17224d8 (main file of fastapi app, create database across asynccontextmanager, CORS, routers)
 
 app.add_middleware(
     CORSMiddleware,
