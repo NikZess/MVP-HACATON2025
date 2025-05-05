22ea5f5c078e997bfa492e706206772a1b39e893
Dmitry <157917126+NikZess@users.noreply.github.com>
2025-05-05 10:00:40 +0300

    menu processing полностью доделано

--- bot/routers/menu_processing.py
+++ bot/routers/menu_processing.py
@@ -1,17 +1,43 @@
 from aiogram import types
 from ..kbds.inline import (
     get_user_main_btns,
+    get_user_tasks_btns,
+    get_user_daily_tasks_btns,
+    get_user_information_btns,
 )
+from ..utils.text_inline import get_description_tasks
+from src.core.models.db_helper import get_async_session
 
-async def main_menu(level: float, menu_name: str, message: types.Message):
-    description = "<strong>Главное меню бота</strong>"
+
+async def main_menu(level: float, menu_name: str):
     kbds = get_user_main_btns(level=level)
-    return description, kbds
+    prefix = "main"
+    return kbds, prefix
 
-async def get_menu_content(
-    level: float,
-    menu_name: str,
-    message: types.Message
-):
+
+async def user_tasks_menu(level: float, menu_name: str):
+    kbds = get_user_tasks_btns(level=level)
+    prefix = "tasks"
+    return kbds, prefix
+
+async def user_daily_tasks_menu(level: float, menu_name: str):
+    kbds = get_user_daily_tasks_btns(level=level)
+    prefix = "daily_tasks"
+    return kbds, prefix
+
+async def user_information_menu(level: float, menu_name: str):
+    kbds = get_user_information_btns(level=level)
+    prefix = "information"
+    return kbds, prefix
+
+
+async def get_menu_content(level: float, menu_name: str):
     if level == 0:
-        return await main_menu(level, menu_name, message)
+        return await main_menu(level, menu_name)
+    if level == 1.0:
+        return await user_tasks_menu(level, menu_name)
+    if level == 2.0:
+        return await user_daily_tasks_menu(level, menu_name)
+    if level == 3.0:
+        return await user_information_menu(level, menu_name)
+
