ea3a309652e596c39ca7d4b57516866311141c1a
Dmitry <157917126+NikZess@users.noreply.github.com>
2025-05-21 20:21:50 +0300

    add user schemas in endpoints

--- src/api/auth/views.py
+++ src/api/auth/views.py
@@ -1,20 +1,24 @@
-from fastapi import APIRouter, Response, Depends
+from fastapi import APIRouter, Response, Depends, Request
+# from fastapi.templating import 
 from sqlalchemy.ext.asyncio import AsyncSession 
 from ...core.models.db_helper import get_async_session
 from . import crud
+from .schemas import User
+from ...core.utils.templates import templates
 
 router = APIRouter(tags=["Authentication"])
 
+from fastapi.responses import JSONResponse
+
 @router.post("/login", summary="Авторизация администратора")
 async def login_admin(
-    username: str,
-    password: str,
-    responce: Response,
+    user_data: User,
+    response: Response,
+    request: Request,
     session: AsyncSession = Depends(get_async_session),
 ):
     return await crud.login_admin(
+        user_data=user_data,
         session=session,
-        username=username,
-        password=password,
-        response=responce
+        response=response,
     )
