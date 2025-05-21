cd81107545013431e0cf4c3355235c5d771488cf
Dmitry <157917126+NikZess@users.noreply.github.com>
2025-05-21 20:22:15 +0300

    schemas for crud funcs

--- src/api/tasks_daily/schemas.py
+++ src/api/tasks_daily/schemas.py
@@ -2,6 +2,6 @@ from pydantic import BaseModel
 from typing import Annotated
 from annotated_types import MinLen, MaxLen
 
-class TaskCreate(BaseModel):
+class TaskDailyCreate(BaseModel):
     username: str
     description: Annotated[str, MinLen(3), MaxLen(1000)]
